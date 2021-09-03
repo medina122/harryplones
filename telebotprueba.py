231650911b7fdc344968dc35c32f9ccafdac351c
medina122 <46516881+medina122@users.noreply.github.com>
2021-09-03 14:02:06 -0500

    Update telebotprueba.py

--- telebotprueba.py
+++ telebotprueba.py
@@ -3,6 +3,13 @@ import telebot
 token = '1759121577:AAHVHZMjB8cFkxzflzcJbNz1C4vLecxzOrg'
 bot = telebot.TeleBot(token)
 chatid =  1711761717
-text = 'hola'
-bot.send_message(chat_id=chatid, text=text)
-print("OK")
+text1 = 'hola'
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
