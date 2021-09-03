6f3702328b5e2539314f8ce8091a9127de75043b
medina122 <46516881+medina122@users.noreply.github.com>
2021-09-03 14:07:03 -0500

    Update telebotprueba.py

--- telebotprueba.py
+++ telebotprueba.py
@@ -4,12 +4,8 @@ token = '1759121577:AAHVHZMjB8cFkxzflzcJbNz1C4vLecxzOrg'
 bot = telebot.TeleBot(token)
 chatid =  1711761717
 text1 = 'hola'
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, "I'm messed up doing those stupid things but i got u back")
 
 bot.polling()
