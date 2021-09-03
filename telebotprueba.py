ff3a004c0df5c5d8c5b1aa7b2002e186592ad7b1
medina122 <46516881+medina122@users.noreply.github.com>
2021-09-03 14:42:09 -0500

    Update telebotprueba.py

--- telebotprueba.py
+++ telebotprueba.py
@@ -1,11 +1,21 @@
 import telebot 
+import logging
+from telebot import apihelper
 
+chatid =  1711761717
+apihelper.SESSION_TIME_TO_LIVE = 5 * 60
 token = '1759121577:AAHVHZMjB8cFkxzflzcJbNz1C4vLecxzOrg'
 bot = telebot.TeleBot(token)
-chatid =  1711761717
-text1 = 'hola'
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "I'm messed up doing those stupid things but i got u back")
 
+@bot.message_handler(commands=['ID'])
+def send_userID(message):
+    chatid = message.chat.id
+    bot.reply_to(message, 'Your ID is %s' % chatid)
+    
 bot.polling()
